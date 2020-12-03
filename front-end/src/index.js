41b20aa2bd47e279b7f57c8910a1b002abb92807
Taylor <tjaroguetech@gmail.com>
2020-12-03 17:52:33 -0500

    Adding functions to generate a single enemy from a collection

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,6 +3,22 @@ const PLAYERS_URL = "http://localhost:3000/users"
 const ENEMY_URL = "http://localhost:3000/enemies"
 let gameArea = document.getElementById("game_area")
 let startButton = document.getElementById("start")
+const enemyCards = []
+startButton.style.display = "none"
+
+debugger
+
+window.addEventListener(`DOMContentLoaded`, (e) => {
+    fetch(ENEMY_URL)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(json) {
+        buildEnemyCards(json)
+    })
+
+    startButton.style.display = "block"
+})
 
 document.getElementById("start").addEventListener("click", startGame)
 
@@ -19,24 +35,34 @@ function displayScores(scorelist) {
     let board = document.getElementById("scoreboard")
     
     let title = document.createElement('h3')
-    title.innerText = `Score for ${scorelist.game_name}`
-    board.appendChild(title) 
+    title.innerText = `Top Scores for ${scorelist.game_name}`
+    board.appendChild(title)
+
+    const top_scores = []
+
     for(const user of scorelist.users) {
         debugger
+        top_scores.push(`${user.username}: ${user.top_score}`)
     }
 
+    top_scores.sort(function(a, b){return b-a})
+    
+    for(let i = 0; i < 10; i++) {
+        let user_score = document.createElement("li")
+        if (top_scores[i]){
+            user_score.innerText = top_scores[i]
+        }
+        else {
+            user_score.innerText = "-"
+        }
+        board.appendChild(user_score)
+    }
 }
 
 function startGame() {
     startButton.style.display = "none"
 
-    fetch(ENEMY_URL)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(json) {
-        buildEnemyCards(json)
-    })
+    drawEnemy(enemyCards)
 }
 
 
@@ -54,7 +80,14 @@ function buildEnemyCards(list) {
         enemyHp.innerText = `HP: ${enemy.hp}`
         enemyChar.appendChild(enemyHp)
 
-        gameArea.appendChild(enemyDiv)
-
+        enemyCards.push(enemyDiv)
+        
     }
+}
+
+function drawEnemy(enemies) {
+    let totalCards = enemyCards.length
+    let i = Math.floor(Math.random() * totalCards)
+    debugger
+    gameArea.appendChild(enemyCards[i])
 }
